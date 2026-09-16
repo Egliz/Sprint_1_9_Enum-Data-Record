a59e44cd718692e2e5d343d7319ef5c8060a263a
Egliz <egliz3010@gmail.com>
2026-09-16 21:46:09 +0200

    feat: add call to the checkDay method

---
+++ src/main/java/level_1/ex1_daysWeek/Main.java
@@ -0,0 +1,10 @@
+package level_1.ex1_daysWeek;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DayChecker.checkDay(Day.MONDAY);
+        DayChecker.checkDay(Day.FRIDAY);
+        DayChecker.checkDay(Day.SATURDAY);
+    }
+}
