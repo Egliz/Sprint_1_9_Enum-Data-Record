a7cb28fdef44ac00145b895dfdda099263470f50
Egliz <egliz3010@gmail.com>
2026-09-16 21:48:18 +0200

    refactor: delete duplicate lines from the switch

--- src/main/java/level_1/ex1_daysWeek/DayChecker.java
+++ src/main/java/level_1/ex1_daysWeek/DayChecker.java
@@ -4,13 +4,11 @@ public class DayChecker {
 
     public static void checkDay(Day day) {
         switch (day) {
-            case MONDAY -> System.out.println(day + " is a workday.");
-            case TUESDAY -> System.out.println(day + " is a workday.");
-            case WEDNESDAY -> System.out.println(day + " is a workday.");
-            case THURSDAY -> System.out.println(day + " is a workday.");
-            case FRIDAY -> System.out.println(day + " is a workday.");
-            case SATURDAY -> System.out.println(day + " is the weekend.");
-            case SUNDAY -> System.out.println(day + " is the weekend.");
+            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY ->
+                    System.out.println(day + " is a workday.");
+
+            case SATURDAY, SUNDAY ->
+                    System.out.println(day + " is the weekend.");
         }
     }
 }
