cc685329acbd4265e871315c7cc1e07d1f90fb6e
Egliz <egliz3010@gmail.com>
2026-09-18 10:46:06 +0200

    feat: implement task levels

---
+++ src/main/java/level_1/ex2_level/Main.java
@@ -0,0 +1,12 @@
+package level_1.ex2_level;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Task task1 = new Task(Level.HIGH);
+        Task task2 = new Task(Level.MEDIUM);
+
+        task1.difficultyLevel();
+        task2.difficultyLevel();
+    }
+}
